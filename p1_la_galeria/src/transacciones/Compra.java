39b2d755c2368c10b106e9cbb4c8f39405d717f1
i53R6IO <141985178+i53R6IO@users.noreply.github.com>
2024-04-26 14:17:45 -0500

    compra, transaccion y subasta -parcial-

--- p1_la_galeria/src/transacciones/Compra.java
+++ p1_la_galeria/src/transacciones/Compra.java
@@ -1,5 +1,92 @@
 package transacciones;
 
-public class Compra {
+import java.util.Date;
+import java.util.HashMap;
+
+import galeria.inventario.ObraDeArte;
+import galeria.usuarios.Usuario;
+
+public class Compra extends Transaccion
+{
+	//heredados
+	private Date fechaSolicitud;
+	private Date fechaAprobacion;
+	private int comprador;
+	private HashMap<Integer, Usuario> Empleados;
+	private HashMap<Integer, Usuario> Clientes;
+	private HashMap<Integer, ObraDeArte> solicitudCompra;
+	private HashMap<Integer, ObraDeArte> solicitudSubasta;
+		
+	
+	//constructor
+	public Compra(Date fechaSolicitud, Date fechaAprobacion, int comprador) 
+	{
+		super(fechaSolicitud, fechaAprobacion, comprador);
+		// TODO Auto-generated constructor stub
+		this.Empleados = new HashMap<Integer, Usuario>( );
+		this.Clientes = new HashMap<Integer, Usuario>( );
+		this.solicitudCompra = new HashMap<Integer, ObraDeArte>( );
+		this.solicitudSubasta = new HashMap<Integer, ObraDeArte>( );
+	}
+
+
+	//Métodos getters
+	
+	@Override
+	public Date getFechaSolicitud() {
+		// TODO Auto-generated method stub
+		return super.getFechaSolicitud();
+	}
+
+	@Override
+	public Date getFechaAprobacion() {
+		// TODO Auto-generated method stub
+		return super.getFechaAprobacion();
+	}
+
+	@Override
+	public int getComprador() {
+		// TODO Auto-generated method stub
+		return super.getComprador();
+	}
+
+	@Override
+	public HashMap<Integer, Usuario> getEmpleados() {
+		// TODO Auto-generated method stub
+		return super.getEmpleados();
+	}
+
+	@Override
+	public HashMap<Integer, Usuario> getClientes() {
+		// TODO Auto-generated method stub
+		return super.getClientes();
+	}
+
+	@Override
+	public HashMap<String, ObraDeArte> getSolicitudCompra() {
+		// TODO Auto-generated method stub
+		return super.getSolicitudCompra();
+	}
+
+	@Override
+	public HashMap<String, ObraDeArte> getSolicitudSubasta() {
+		// TODO Auto-generated method stub
+		return super.getSolicitudSubasta();
+	}
+	
+	
+	//Métodos específicos
+	
+	public boolean verificarCompra()
+	{
+		// TODO terminar
+		return true;
+	}
+	
+	public boolean aprobarCompra()
+	{
+		// TODO terminar
+		return true;
+	}
 
 }
--- p1_la_galeria/src/transacciones/Pago.java
+++ p1_la_galeria/src/transacciones/Pago.java
@@ -1,5 +1,25 @@
 package transacciones;
 
-public class Pago {
+import java.util.ArrayList;
 
+public class Pago {
+	
+	private String tipoPago;
+	private int valor;
+	private int identificacion_cliente;
+	private int codigoRegistro;
+
+	public Pago(String tipoPago, int valor, int identificacion_cliente, int codigoRegistro) {
+		super();
+		this.tipoPago = tipoPago;
+		this.valor = valor;
+		this.identificacion_cliente = identificacion_cliente;
+		this.codigoRegistro = codigoRegistro;
+	}
+	public String getTipoPago() {
+		return tipoPago;
+	}
+	public void setTipoPago(String tipoPago) {
+		this.tipoPago = tipoPago;
+	}
 }
